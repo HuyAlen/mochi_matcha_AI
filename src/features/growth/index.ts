85fd8dbee12964c423a79fdd7258d4b3f178e664
HuyAlen <ndhuy211094@gmail.om>
2026-06-25 22:15:18 +0700

    Sprint 13.7: fix growth barrel exports

--- src/features/growth/index.ts
+++ src/features/growth/index.ts
@@ -1,13 +1,14 @@
-﻿export * from "./components/GrowthBabySelector";
-export * from "./components/GrowthChartCard";
-export * from "./components/GrowthEntryManager";
-export * from "./components/GrowthEntrySheet";
-export * from "./components/GrowthInsightCard";
-export * from "./components/GrowthMetricTabs";
-export * from "./components/GrowthPredictionCard";
-export * from "./components/GrowthStatsCard";
-export * from "./components/GrowthTabs";
-export * from "./components/GrowthTrendCard";
-export * from "./components/TwinGrowthCompare";
-export * from "./components/WHOChartCard";
+﻿export { default as GrowthBabySelector } from "./components/GrowthBabySelector";
+export { default as GrowthChartCard } from "./components/GrowthChartCard";
+export { default as GrowthEntryManager } from "./components/GrowthEntryManager";
+export { default as GrowthEntrySheet } from "./components/GrowthEntrySheet";
+export { default as GrowthInsightCard } from "./components/GrowthInsightCard";
+export { default as GrowthMetricTabs } from "./components/GrowthMetricTabs";
+export { default as GrowthPredictionCard } from "./components/GrowthPredictionCard";
+export { default as GrowthStatsCard } from "./components/GrowthStatsCard";
+export { default as GrowthTabs } from "./components/GrowthTabs";
+export { default as GrowthTrendCard } from "./components/GrowthTrendCard";
+export { default as TwinGrowthCompare } from "./components/TwinGrowthCompare";
+export { default as WHOChartCard } from "./components/WHOChartCard";
+
 export * from "./store/growthStore";
