dc4b466d009685415fc2cc8ac915ef9933e3154c
HuyAlen <ndhuy211094@gmail.om>
2026-06-25 22:20:24 +0700

    Sprint 13.7: fix vaccines barrel exports

--- src/features/vaccines/index.ts
+++ src/features/vaccines/index.ts
@@ -1,9 +1,9 @@
-﻿export * from "./components/UpcomingVaccineCard";
-export * from "./components/VaccineBabySelector";
-export * from "./components/VaccineInsightCard";
-export * from "./components/VaccineReactionList";
-export * from "./components/VaccineReactionTracker";
-export * from "./components/VaccineScheduleList";
-export * from "./components/VaccineSummaryCard";
+﻿export { default as UpcomingVaccineCard } from "./components/UpcomingVaccineCard";
+export { default as VaccineBabySelector } from "./components/VaccineBabySelector";
+export { default as VaccineInsightCard } from "./components/VaccineInsightCard";
+export { default as VaccineReactionList } from "./components/VaccineReactionList";
+export { default as VaccineReactionTracker } from "./components/VaccineReactionTracker";
+export { default as VaccineScheduleList } from "./components/VaccineScheduleList";
+export { default as VaccineSummaryCard } from "./components/VaccineSummaryCard";
 
 export * from "./store/vaccineStore";
