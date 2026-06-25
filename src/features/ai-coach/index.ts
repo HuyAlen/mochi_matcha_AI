4d2b0cffbaff09ead682fecd385564ce5d145ce4
HuyAlen <ndhuy211094@gmail.om>
2026-06-25 22:22:20 +0700

    Sprint 13.7: fix ai coach barrel exports

--- src/features/ai-coach/index.ts
+++ src/features/ai-coach/index.ts
@@ -1,7 +1,7 @@
-﻿export * from "./components/AIChatInput";
-export * from "./components/AIChatMessages";
-export * from "./components/AIDataSnapshot";
-export * from "./components/AIHeroCard";
-export * from "./components/AIInsightPanel";
-export * from "./components/AISuggestionList";
-export * from "./components/AITwinSummaryCard";
+﻿export { default as AIChatInput } from "./components/AIChatInput";
+export { default as AIChatMessages } from "./components/AIChatMessages";
+export { default as AIDataSnapshot } from "./components/AIDataSnapshot";
+export { default as AIHeroCard } from "./components/AIHeroCard";
+export { default as AIInsightPanel } from "./components/AIInsightPanel";
+export { default as AISuggestionList } from "./components/AISuggestionList";
+export { default as AITwinSummaryCard } from "./components/AITwinSummaryCard";
