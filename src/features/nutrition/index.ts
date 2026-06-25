c925febb20c1f40ecc5277b753da93721f062cf6
HuyAlen <ndhuy211094@gmail.om>
2026-06-25 22:17:48 +0700

    Sprint 13.7: fix nutrition barrel exports

--- src/features/nutrition/index.ts
+++ src/features/nutrition/index.ts
@@ -1,11 +1,12 @@
-﻿export * from "./components/BabyMealSelector";
-export * from "./components/FoodLibraryList";
-export * from "./components/MealEntryQuickAdd";
-export * from "./components/MealHistoryCard";
-export * from "./components/MealSuggestionGrid";
-export * from "./components/NutritionInsightCard";
-export * from "./components/NutritionScoreCard";
-export * from "./components/NutritionTabs";
-export * from "./components/ShoppingListCard";
-export * from "./components/WeeklyMenuCard";
+﻿export { default as BabyMealSelector } from "./components/BabyMealSelector";
+export { default as FoodLibraryList } from "./components/FoodLibraryList";
+export { default as MealEntryQuickAdd } from "./components/MealEntryQuickAdd";
+export { default as MealHistoryCard } from "./components/MealHistoryCard";
+export { default as MealSuggestionGrid } from "./components/MealSuggestionGrid";
+export { default as NutritionInsightCard } from "./components/NutritionInsightCard";
+export { default as NutritionScoreCard } from "./components/NutritionScoreCard";
+export { default as NutritionTabs } from "./components/NutritionTabs";
+export { default as ShoppingListCard } from "./components/ShoppingListCard";
+export { default as WeeklyMenuCard } from "./components/WeeklyMenuCard";
 
+export * from "./store/mealStore";
